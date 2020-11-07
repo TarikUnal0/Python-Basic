1f36d13ee18aecd576a7e97896baf4d172dcab01
Tarik Ünal <2108287@talnet.nl>
2020-11-07 16:49:51 +0100

    3 opdr

---
+++ 1-Introductie/taak02-datatypes/index.py
@@ -0,0 +1,6 @@
+a = "iets"
+b = "anders"
+a = a + b
+
+for x in range(1,20):
+    print(a)
---
+++ 2-vervolg datatypes/taak01-lists/index.py
@@ -0,0 +1,2 @@
+maanden = ["januari","februari","maart","april","mei","juni","july","augustus","september",'oktober',"november","december"]
+print(maanden)
